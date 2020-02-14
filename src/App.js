5358786bb83fc2c3a031befcecc56488eb327f91
Brian Blosser <brian.blosser@wwt.com>
2020-02-13 21:12:58 -0500

    Make loop run one time.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,49 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [startTime, setStartTime] = useState('0');
-  
-  const audioPlayerRef = useRef(); 
+  const [startTime, setStartTime] = useState(0);
+
+  const audioPlayerRef = useRef();
   const handleStartLoop = () => {
     audioPlayerRef.current.fastSeek(startTime);
     audioPlayerRef.current.play();
-    audioPlayerRef.current.pause();
   };
-  
+
   const onValueChange = (event, setterFunction) => {
-    setterFunction(event.target.value);
+    setterFunction(parseInt(event.target.value));
   };
-  
-  const onProgress = (event) => {
-    console.log(event);
+
+  const onTimeUpdate = event => {
+    if (audioPlayerRef.current.currentTime >= 2) {
+      audioPlayerRef.current.pause();
+    }
   };
-  
-  const onTimeUpdate = (event) => {
-    console.log(audioPlayerRef.current.currentTime);
-  };
-  
+
   return (
     <div className="App">
       <main>
-        <audio 
-          ref={audioPlayerRef} 
-          data-testid={'audio-player'} 
-          controls 
+        <audio
+          ref={audioPlayerRef}
+          data-testid={"audio-player"}
+          controls
           src="la-saison-des-caucus-et-des-primaires-aux-eta.mp4"
-          onProgress={onProgress}
           onTimeUpdate={onTimeUpdate}
         >
           Your browser does not support the
           <code>audio</code> element.
         </audio>
         <label htmlFor="loop-start-time">Loop Start Time</label>
-        <input 
-          id="loop-start-time" 
+        <input
+          id="loop-start-time"
           type="number"
           value={startTime}
-          onChange={(event) => {
-            onValueChange(event,setStartTime);
+          onChange={event => {
+            onValueChange(event, setStartTime);
           }}
         />
         <label htmlFor="loop-end-time">Loop End Time</label>
-        <input id="loop-end-time" type="number"/>
+        <input id="loop-end-time" type="number" />
         <button onClick={handleStartLoop}>Start Loop</button>
       </main>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,46 +1,89 @@
-import React from 'react';
-import { render, wait } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, wait } from "@testing-library/react";
+import App from "./App";
 import userEvent from "@testing-library/user-event";
 
-describe('App', () => {
-  test('allows you to set a loop start and end', async () => {
-    const startTime = '5';
-    const endTime = '6';
-    
+Object.defineProperty(window.HTMLMediaElement.prototype, "duration", {
+  value: 4,
+  writable: true
+});
+Object.defineProperty(window.HTMLMediaElement.prototype, "currentTime", {
+  value: 0,
+  writable: true
+});
+Object.defineProperty(window.HTMLMediaElement.prototype, "paused", {
+  value: true,
+  writable: true
+});
+Object.defineProperty(window.HTMLMediaElement.prototype, "ended", {
+  value: false,
+  writable: true
+});
+window.HTMLMediaElement.prototype.advanceAudioPlayer = function() {
+  const timeIncrement = 0.1;
+  if (Math.floor(this.currentTime) === this.duration) {
+    this.ended = true;
+    return;
+  }
+  if (!this.paused && !this.ended) {
+    this.currentTime += timeIncrement;
+    this.dispatchEvent(new window.Event("timeupdate"));
+    setTimeout(() => {
+      this.advanceAudioPlayer();
+    }, timeIncrement * 1000);
+  }
+};
+window.HTMLMediaElement.prototype.fastSeek = function(time) {
+  this.currentTime = time;
+};
+window.HTMLMediaElement.prototype.play = function() {
+  this.paused = false;
+  this.advanceAudioPlayer();
+};
+window.HTMLMediaElement.prototype.pause = function() {
+  this.paused = true;
+};
+
+describe("App", () => {
+  test("allows you to play one iteration of a loop", async () => {
+    // Arrange
+    jest.useFakeTimers();
+    const startTimeInputValue = 1;
+    const endTimeInputValue = 2;
+    const loopLength = endTimeInputValue - startTimeInputValue;
+
+    const playSpy = jest
+      .spyOn(window.HTMLMediaElement.prototype, "play")
+      .mockName("play");
+    const fastSeekSpy = jest
+      .spyOn(window.HTMLMediaElement.prototype, "fastSeek")
+      .mockName("fastSeek");
+    const pauseSpy = jest
+      .spyOn(window.HTMLMediaElement.prototype, "pause")
+      .mockName("pause");
+
+    // Act
     const { findByTestId, findByLabelText, findByText } = render(<App />);
-    const audioPlayer = await findByTestId('audio-player');
-    console.log(audioPlayer);
-    // audioPlayer.play = jest.fn().mockName('audioPlayer.play');
-    jest.spyOn(audioPlayer.play);
-    // audioPlayer.fastSeek = jest.fn().mockName('audioPlayer.fastSeek');
-    jest.spyOn(audioPlayer.fastSeek);
-    // audioPlayer.pause = jest.fn().mockName('audioPlayer.pause');
-    jest.spyOn(audioPlayer.pause);
-    
+
+    const audioPlayer = await findByTestId("audio-player");
     const loopStartTimeElement = await findByLabelText(/loop start time/i);
     const loopEndTimeElement = await findByLabelText(/loop end time/i);
-
     const startLoopButton = await findByText(/start loop/i);
-    
-    await userEvent.type(loopStartTimeElement, startTime);
-    await userEvent.type(loopEndTimeElement, endTime);
+
+    await userEvent.type(loopStartTimeElement, String(startTimeInputValue));
+    await userEvent.type(loopEndTimeElement, String(endTimeInputValue));
     await userEvent.click(startLoopButton);
-    await wait(() => {
-      expect(audioPlayer.fastSeek).toHaveBeenCalledTimes(1);
-      expect(audioPlayer.fastSeek).toHaveBeenCalledWith(startTime);
-    });
-    await wait(() => {
-      expect(audioPlayer.play).toHaveBeenCalledTimes(1);
-    });
-    
-    await wait(() => {
-      expect(audioPlayer.pause).toHaveBeenCalledTimes(1);
-    });
-    
-    expect(audioPlayer.currentTime).toBeCloseTo(endTime, 1);
+
+    // Assert
+    expect(fastSeekSpy).toHaveBeenCalledTimes(1);
+    expect(fastSeekSpy).toHaveBeenCalledWith(startTimeInputValue);
+    expect(playSpy).toHaveBeenCalledTimes(1);
+
+    // Act
+    jest.advanceTimersByTime(loopLength * 1000);
+
+    // Assert
+    expect(pauseSpy).toHaveBeenCalledTimes(1);
+    expect(Math.round(audioPlayer.currentTime)).toBe(endTimeInputValue);
   });
-
-  
 });
-
