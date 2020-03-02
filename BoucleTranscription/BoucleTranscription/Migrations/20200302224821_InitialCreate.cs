c1bbd8704c1d9b49106dc8356c81b22dc42bb4ec
Brian Blosser <brian.blosser@wwt.com>
2020-03-02 17:56:11 -0500

    Create initial database migration.

---
+++ BoucleTranscription/BoucleTranscription/Migrations/20200302224821_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BoucleTranscription.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clip",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    start_time = table.Column<int>(nullable: false),
+                    end_time = table.Column<int>(nullable: false),
+                    transcription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clip", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "clip");
+        }
+    }
+}
