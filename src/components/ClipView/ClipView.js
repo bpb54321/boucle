87bae08d4fa0069d8ac0dfcf4c5470bda768af3c
Brian Blosser <brian.blosser@wwt.com>
2020-03-09 09:55:38 -0400

    Get clip transcription from the server using axios.

--- src/components/ClipView/ClipView.js
+++ src/components/ClipView/ClipView.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 export function ClipView() {
+  const [transcription, setTranscription] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get();
+      setTranscription(response.data.transcription);
+    })();
+  });
   return (
     <input
       data-testid="transcription-input"
-      defaultValue="test transcription"
+      type="textarea"
+      onChange={() => {
+        setTranscription(transcription);
+      }}
+      value={transcription}
     />
   );
 }
