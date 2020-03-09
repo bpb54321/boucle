1e324c8078d56d8c42e7daa55ec1326eadc6524b
Brian Blosser <brian.blosser@wwt.com>
2020-03-09 09:41:54 -0400

    Add test-data-bot to test.

--- src/components/ClipView/ClipView.test.js
+++ src/components/ClipView/ClipView.test.js
@@ -1,16 +1,27 @@
 import { ClipView } from "components/ClipView/ClipView";
 import { render } from "@testing-library/react";
 import React from "react";
+import { build, fake } from "@jackfranklin/test-data-bot";
+import axios from "axios";
 
+jest.mock("axios");
 describe("ClipView", () => {
   test("should display clip properties from the API", async () => {
     // Arrange
+    const clipBuilder = build("Clip", {
+      fields: {
+        transcription: fake(f => f.lorem.lines(2))
+      }
+    });
+    const clip = clipBuilder();
+    axios.get.mockResolvedValue({ data: clip });
+
     const { findByTestId } = render(<ClipView />);
 
     // Act
     const transcriptionInput = await findByTestId("transcription-input");
 
     // Assert
-    expect(transcriptionInput).toHaveValue("test transcription");
+    expect(transcriptionInput).toHaveValue(clip.transcription);
   });
 });
