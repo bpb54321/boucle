e0b3acd68a8da5d779426fe93e2d329e51e828d7
Brian Blosser <brian.blosser@wwt.com>
2020-04-12 15:20:31 -0400

    Put ClipPlayer in its own component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { ClipEditForm } from "components/ClipEditForm/ClipEditForm";
-import React, { useRef, useState, useEffect, useCallback } from "react";
+import { ClipPlayer } from "components/ClipPlayer";
+import React, { useEffect } from "react";
 import "App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { getClips, getCurrentClipIndex } from "redux/clip/clipSelectors";
-import { clipChanged } from "redux/clip/clipSlice";
 import { clipDefaultDuration } from "constants.js";
 import { clipAdded } from "redux/clips/clipsSlice";
 import { fetchClips } from "redux/clips/clipsThunks";
@@ -23,47 +23,11 @@ const App = () => {
       transcription: ""
     };
   }
-  const [isStopped, setIsStopped] = useState(false);
-  const [finishedBreak, setFinishedBreak] = useState(false);
-  const [pauseTimeBetweenLoops, setPauseTimeBetweenLoops] = useState(0);
-
-  const audioPlayerRef = useRef();
-  const handleStartLoop = useCallback(() => {
-    audioPlayerRef.current.currentTime = clip.startTime;
-    audioPlayerRef.current.play();
-  }, [clip.startTime]);
-
-  function handleStopLoop() {
-    audioPlayerRef.current.pause();
-    setIsStopped(true);
-  }
-
-  useEffect(() => {
-    if (!isStopped && finishedBreak) {
-      handleStartLoop();
-    }
-  }, [isStopped, finishedBreak, handleStartLoop]);
-
-  useEffect(() => {
-    const loopDuration = clip.endTime - clip.startTime;
-    setPauseTimeBetweenLoops(loopDuration);
-  }, [clip.startTime, clip.endTime]);
 
   useEffect(() => {
     window.lastAction = "clips fetched";
     dispatch(fetchClips());
   }, [dispatch]);
-
-  const onTimeUpdate = () => {
-    if (Math.floor(audioPlayerRef.current.currentTime) === clip.endTime) {
-      audioPlayerRef.current.pause();
-      setFinishedBreak(false);
-
-      setTimeout(() => {
-        setFinishedBreak(true);
-      }, pauseTimeBetweenLoops * 1000);
-    }
-  };
 
   const handleAddClip = () => {
     if (clips.length > 0) {
@@ -92,27 +56,10 @@ const App = () => {
       data-testid={"app"}
     >
       <main>
-        <audio
-          ref={audioPlayerRef}
-          data-testid={"audio-player"}
-          controls
-          src="../public/infoman-s20-e24.mp4"
-          onTimeUpdate={onTimeUpdate}
-        >
-          Your browser does not support the
-          <code>audio</code> element.
-        </audio>
+        <ClipPlayer clip={clip} />
         <div>
           <button data-testid={"new-clip-button"} onClick={handleAddClip}>
             New Clip
-          </button>
-        </div>
-        <div>
-          <button onClick={handleStartLoop} data-testid="start-loop-button">
-            Start
-          </button>
-          <button onClick={handleStopLoop} data-testid="stop-loop-button">
-            Stop
           </button>
         </div>
         {clips.length > 0 ? <ClipEditForm clip={clip} /> : null}
---
+++ src/components/ClipPlayer.js
@@ -0,0 +1,63 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+export const ClipPlayer = ({ clip }) => {
+  const [isStopped, setIsStopped] = useState(false);
+  const [finishedBreak, setFinishedBreak] = useState(false);
+  const [pauseTimeBetweenLoops, setPauseTimeBetweenLoops] = useState(0);
+
+  const audioPlayerRef = useRef();
+  const handleStartLoop = useCallback(() => {
+    audioPlayerRef.current.currentTime = clip.startTime;
+    audioPlayerRef.current.play();
+  }, [clip.startTime]);
+
+  function handleStopLoop() {
+    audioPlayerRef.current.pause();
+    setIsStopped(true);
+  }
+
+  const onTimeUpdate = () => {
+    if (Math.floor(audioPlayerRef.current.currentTime) === clip.endTime) {
+      audioPlayerRef.current.pause();
+      setFinishedBreak(false);
+
+      setTimeout(() => {
+        setFinishedBreak(true);
+      }, pauseTimeBetweenLoops * 1000);
+    }
+  };
+
+  useEffect(() => {
+    if (!isStopped && finishedBreak) {
+      handleStartLoop();
+    }
+  }, [isStopped, finishedBreak, handleStartLoop]);
+
+  useEffect(() => {
+    const loopDuration = clip.endTime - clip.startTime;
+    setPauseTimeBetweenLoops(loopDuration);
+  }, [clip.startTime, clip.endTime]);
+
+  return (
+    <div>
+      <audio
+        ref={audioPlayerRef}
+        data-testid={"audio-player"}
+        controls
+        src="../public/infoman-s20-e24.mp4"
+        onTimeUpdate={onTimeUpdate}
+      >
+        Your browser does not support the
+        <code>audio</code> element.
+      </audio>
+      <div>
+        <button onClick={handleStartLoop} data-testid="start-loop-button">
+          Start
+        </button>
+        <button onClick={handleStopLoop} data-testid="stop-loop-button">
+          Stop
+        </button>
+      </div>
+    </div>
+  );
+};
