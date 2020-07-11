c8473df5bbf2e03652b0cf5a10a52d6fe116d9b9
Brian Blosser <brian.blosser@wwt.com>
2020-07-11 15:45:47 -0400

    Update mp3 url.

--- src/components/ClipPlayer.js
+++ src/components/ClipPlayer.js
@@ -44,7 +44,7 @@ export const ClipPlayer = () => {
         ref={audioPlayerRef}
         data-testid={"audio-player"}
         controls
-        src="lecon_1_dialogue_0.mp3"
+        src="https://www.japprendslequebecois.com/sites/default/files/mp3/lecon_4_dialogue.mp3"
         onTimeUpdate={onTimeUpdate}
       >
         Your browser does not support the
