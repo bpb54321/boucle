71a03cdd7fa52b9305a16cd5262dedd52c8074c5
Brian Blosser <brian.blosser@wwt.com>
2020-07-15 16:35:35 -0400

    Update ClipAdder to use simplified action.

--- src/components/ClipAdder.js
+++ src/components/ClipAdder.js
@@ -1,32 +1,14 @@
 import { clipDefaultDuration } from "constants.js";
 import React from "react";
-import { addClip } from "redux/clips/clipsThunks";
+import { clipAdded } from "../redux/actions";
 import { useSelector, useDispatch } from "react-redux";
 import { getClip, getClips } from "redux/selectors";
 
 export const ClipAdder = () => {
   const dispatch = useDispatch();
-  const clips = useSelector(getClips);
-  const clip = useSelector(getClip);
 
   const handleAddClip = () => {
-    if (clips.length > 0) {
-      dispatch(
-        addClip({
-          startTime: clip.endTime,
-          endTime: clip.endTime + clipDefaultDuration,
-          transcription: ""
-        })
-      );
-    } else {
-      dispatch(
-        addClip({
-          startTime: 0,
-          endTime: 5,
-          transcription: ""
-        })
-      );
-    }
+    dispatch(clipAdded());
   };
 
   return (
