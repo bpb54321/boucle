839791d981bbf7638596cd5b6f9413988b369c88
Brian Blosser <brian.blosser@wwt.com>
2020-07-15 16:35:59 -0400

    Add new reducer to root reducer.

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { clipsReducer } from "redux/clips/clipsSlice";
 import { clipReducer } from "redux/clip/clipSlice";
+import clips from "redux/clips/clips";
 
 export const rootReducer = combineReducers({
   clipsSlice: clipsReducer,
-  clipSlice: clipReducer
+  clipSlice: clipReducer,
+  clips
 });
